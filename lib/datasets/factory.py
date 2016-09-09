a77db4411242dd8a7d94d9f35a3c4e0913c1ffca
SoonminHwang <jjang9hsm@gmail.com>
2016-09-09 02:42:05 +0200

    Add imdb classes for pedestrian detection
    Not completed. (caltech_ped, kaist_ped)
    caltech_ped.py / kaist_ped.py / layer.py / train.py

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,9 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.kaist_ped import kaist_ped
+from datasets.caltech_ped import caltech_ped
+
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -31,6 +34,19 @@ for year in ['2015']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
 
+# Set up kaist_ped_2015_<split>
+for year in ['2015']:	
+	for split in ['train20', 'train02', 'test20', 'test01']:	# test01: to generate result videos
+        name = 'kaist_ped_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: kaist_ped(split, year))
+		
+# Set up caltech_ped_2009_<split>
+for year in ['2009']:	
+	for split in ['train30', 'train04', 'test30', 'test01']:	# test01: to generate result videos
+        name = 'caltech_ped_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: caltech_ped(split, year))
+
+		
 def get_imdb(name):
     """Get an imdb (image database) by name."""
     if not __sets.has_key(name):
